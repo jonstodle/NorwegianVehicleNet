13e0300935d40f069a77ac10738bec38cf8ad1e2
Jon Stødle <jonstodle@outlook.com>
2015-10-08 13:23:44 +0200

    Initial commit

---
+++ NorwegianVehicleNet/SVVClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace NorwegianVehicleNet
+{
+    public sealed class SVVClient
+    {
+        private const string SvvBaseUriString = "http://www.vegvesen.no/system/mobilapi?registreringsnummer=";
+
+        private XDocument GetXmlDocument()
+    }
+}
