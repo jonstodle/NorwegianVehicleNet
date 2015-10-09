83dab0ea4cca72ed6569e9e763a5c08e1e266489
Jon Stødle <jonstodle@outlook.com>
2015-10-09 08:00:30 +0200

    Add engine

---
+++ NorwegianVehicleNet/Car/Engine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorwegianVehicleNet.Car
+{
+    public class Engine
+    {
+        public int SlagVolum { get; set; }
+        public int Liter { get; set; }
+        public int MotorYtelse { get; set; }
+        public string MotorYtelseBenevning { get; set; }
+        public int HesteKrefter { get; set; }
+    }
+}
