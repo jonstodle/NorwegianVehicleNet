7354188a115124ad6a31bf0be9bcd69beca6a449
Jon Stødle <jonstodle@outlook.com>
2015-10-09 07:43:00 +0200

    Add car namespace

--- NorwegianVehicleNet/Car/RegistrationNumber.cs
+++ NorwegianVehicleNet/Car/RegistrationNumber.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace NorwegianVehicleNet
+namespace NorwegianVehicleNet.Car
 {
     public class RegistrationNumber
     {
