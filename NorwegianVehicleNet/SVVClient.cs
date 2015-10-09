3a76199d5571712a9a342d1bd182a00410409dec
Jon Stødle <jonstodle@outlook.com>
2015-10-09 07:51:39 +0200

    Add xdocument getter

--- NorwegianVehicleNet/SVVClient.cs
+++ NorwegianVehicleNet/SVVClient.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using NorwegianVehicleNet.Car;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
@@ -11,6 +13,16 @@ namespace NorwegianVehicleNet
     {
         private const string SvvBaseUriString = "http://www.vegvesen.no/system/mobilapi?registreringsnummer=";
 
-        //private XDocument GetXmlDocument();
+        /// <summary>
+        /// Gets the xml representation of a car
+        /// </summary>
+        /// <param name="registrationNumber">The registration number of the car to get</param>
+        /// <returns>An XDocument representing the car</returns>
+        private async Task<XDocument> GetXmlDocumentAsync(RegistrationNumber registrationNumber)
+        {
+            var client = new HttpClient();
+            var xmlString = await client.GetStringAsync(SvvBaseUriString + registrationNumber);
+            return XDocument.Parse(xmlString);
+        }
     }
 }
