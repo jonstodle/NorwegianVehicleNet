8f409c5bd962a5577390e84e8d181a9c05568410
Jon Stødle <jonstodle@outlook.com>
2015-10-09 08:05:21 +0200

    Start work on car class

---
+++ NorwegianVehicleNet/Car/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorwegianVehicleNet.Car
+{
+    class Car
+    {
+        public enum FuelTypes { Unknown, Gasoline, Diesel, Electricity };
+
+        public string Merke { get; set; }
+        public string Modell { get; set; }
+        public string Brukstypegruppe { get; set; }
+        public string Brukstypeikon { get; set; }
+        public string Farge { get; set; }
+        public string Kjoretoysklasse { get; set; }
+        public int Seter { get; set; }
+        public int Staaplasser { get; set; }
+        public FuelTypes DrivstoffType { get; set; }
+        public Engine Motor { get; set; }
+        public int AkslerMedDrift { get; set; }
+        public int EgenvektMedForer { get; set; }
+        public int TilhengervektMedBrems { get; set; }
+        public int TilhengervektUtenBrems { get; set; }
+        public int TilhengervektKopling { get; set; }
+        public int Vogntogvekt { get; set; }
+        public int Taklast { get; set; }
+        public RegistrationNumber Registreringsnummer { get; set; }
+        public string Understellsnummer { get; set; }
+    }
+}
