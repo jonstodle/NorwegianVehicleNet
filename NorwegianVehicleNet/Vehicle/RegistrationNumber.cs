19bbe70f3334488254b729a5475789da3b516308
Jon Stødle <jonstodle@outlook.com>
2015-10-09 23:43:37 +0200

    Change namespace from car to vehicle

--- NorwegianVehicleNet/Vehicle/RegistrationNumber.cs
+++ NorwegianVehicleNet/Vehicle/RegistrationNumber.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace NorwegianVehicleNet.Car
+namespace NorwegianVehicleNet.Vehicle
 {
     public class RegistrationNumber
     {
